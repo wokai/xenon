69c2063a757c13aa726ec1636e5c715b406d0be6
Wolfgang Kaisers <wolfgang.kaisers@gmail.com>
2023-03-25 13:24:48 +0100

    updated text

--- model/data/text.js
+++ model/data/text.js
@@ -25,7 +25,7 @@ const path = require('path');
 const config              = require(path.join(__dirname, '..', '..', 'config', 'general'));
 const win                 = require(path.join(__dirname, '..', '..', 'logger', 'logger'));
 const bus                 = require(path.join(__dirname, '..', '..', 'config', 'medibus'));
-const { episode }         = require(path.join(__dirname, 'episode'));
+const { episode }         = require(path.join(__dirname, '..', 'episode'));
 const TextMessageResponse = require(path.join(__dirname, '..', 'medibus', 'textMessageResponse'));
 
 const { TimePoint, ParameterElement, ParameterMap } = require(path.join(__dirname, 'parameterMap'));
